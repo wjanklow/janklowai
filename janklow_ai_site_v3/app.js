520cb80bfbefed4ec009de6fe1ece1fc5619816b
wjanklow <wjanklow@gmail.com>
2025-05-02 18:14:23 -0500

    Delete janklow_ai_site_v3 directory

--- janklow_ai_site_v3/app.js
+++
@@ -1,7 +0,0 @@
-// smooth scroll for internal anchor links
-document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
-  anchor.addEventListener('click',function(e){
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({behavior:'smooth'});
-  });
-});
